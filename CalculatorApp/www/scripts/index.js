46d76a4a061f53d12d70e55cf944b90699f7ed66
mattl <mattl@DESKTOP-OHKJRKK>
2017-02-01 12:43:10 -0500

    Added missing comments.

--- CalculatorApp/www/scripts/index.js
+++ CalculatorApp/www/scripts/index.js
@@ -1,8 +1,9 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-
+﻿/* 
+Author: Matthew Lane (200214586)
+DOC: 01/02/2017
+Purpose: A calculator program, written in HTML and JS to be run on an android emulator (cordova).
+File: This is the JavaScript area of the application. The buttons are connected and the calculations are performed. 
+*/
 
 //Globals
 var numberOne = 0;
@@ -15,7 +16,9 @@ var type = null;
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
-    //Connect the buttons an watch for a click. 
+   /* Connect the buttons and watch for a user press */
+
+    //Connect the number buttons
     document.getElementById("0").onclick = function () { if (display.value == 0 || display.value == "ERR") display.value = ""; display.value += "0"; if (nextValue == false) numberOne = ((numberOne * 10) + 0); else numberTwo = ((numberTwo * 10) + 0); };
     document.getElementById("1").onclick = function () { if (display.value == 0 || display.value == "ERR") display.value = ""; display.value += "1"; if (nextValue == false) numberOne = ((numberOne * 10) + 1); else numberTwo = ((numberTwo * 10) + 1); };
     document.getElementById("2").onclick = function () { if (display.value == 0 || display.value == "ERR") display.value = ""; display.value += "2"; if (nextValue == false) numberOne = ((numberOne * 10) + 2); else numberTwo = ((numberTwo * 10) + 2); };
@@ -27,6 +30,9 @@ var type = null;
     document.getElementById("8").onclick = function () { if (display.value == 0 || display.value == "ERR") display.value = ""; display.value += "8"; if (nextValue == false) numberOne = ((numberOne * 10) + 8); else numberTwo = ((numberTwo * 10) + 8); };
     document.getElementById("9").onclick = function () { if (display.value == 0 || display.value == "ERR") display.value = ""; display.value += "9"; if (nextValue == false) numberOne = ((numberOne * 10) + 9); else numberTwo = ((numberOne * 10) + 9); };
 
+    /*Connect the evaluators */
+
+    //Clear the numbers, evaluators and display
     document.getElementById("C").onclick = function () { display.value = 0; numberOne = 0; numberTwo = 0; nextValue = false; type = null; }; 
     document.getElementById("/").onclick = function () { display.value = 0; type = "/"; nextValue = true; };
     document.getElementById("X").onclick = function () { display.value = 0; type = "X"; nextValue = true; };
@@ -34,13 +40,16 @@ var type = null;
     document.getElementById("-").onclick = function () { display.value = 0; type = "-"; nextValue = true; };
     document.getElementById("=").onclick = calculate;
 
+    //This functions handles the calculations when the = button is pressed
     function calculate() {
 
+        //We have performed a calculation, so we are back to the first number. 
         nextValue = false;
 
+        //Switch on what evaluator was pressed
         switch (type)
         {
-            case "+":
+            case "+"://Addition
                 if (numberTwo != 0) {
                     display.value = (numberOne + numberTwo);
                     numberOne = (numberOne + numberTwo);
@@ -48,36 +57,31 @@ var type = null;
                 }
                 break;
 
-            case "-":
+            case "-"://Subtraction
                 display.value = (numberOne - numberTwo);
                 numberOne = (numberOne - numberTwo);
                 numberTwo = 0;
                 break;
 
-            case "X":
+            case "X"://Multiplication
                 display.value = (numberOne * numberTwo);
                 numberOne = (numberOne * numberTwo);
                 numberTwo = 0;
                 break;
 
-            case "/":
+            case "/"://Division
                 if (numberOne != 0 && numberTwo != 0) {
                     display.value = (numberOne / numberTwo);
                     numberOne = (numberOne / numberTwo);
                     numberTwo = 0;
                 }
-                else{
+                else{//Caught an error
                     display.value = "ERR";
                     numberOne = 0;
                     numberTwo = 0;
                     nextValue = false;
                     type = null;
                 }
-
-                break;
-
-            case "SQRT":
-
                 break;
         }
     }
